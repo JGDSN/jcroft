ac3c99b9b390f4baad6eb4d60c9ee93a57705bcf
FriwiDev <bblosaurus@web.de>
2018-07-19 06:03:07 +0200

    Remove unused imports

--- src/test/java/de/agdsn/jcroft/hazelcast/HazelJetTest.java
+++ src/test/java/de/agdsn/jcroft/hazelcast/HazelJetTest.java
@@ -1,14 +1,7 @@
 package de.agdsn.jcroft.hazelcast;
 
-import de.agdsn.jcroft.JCroftConfiguration;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
 
 public class HazelJetTest {
 
