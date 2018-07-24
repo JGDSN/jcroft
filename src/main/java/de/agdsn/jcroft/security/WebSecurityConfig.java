6030c6876534a265cd3774b1539a5b1e2ae170f1
FriwiDev <bblosaurus@web.de>
2018-07-24 21:57:12 +0200

    Disable CSRF protection (blocks login request, got to investigate that later) Add small icon to software logo

--- src/main/java/de/agdsn/jcroft/security/WebSecurityConfig.java
+++ src/main/java/de/agdsn/jcroft/security/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
                 .formLogin()
                 .loginPage("/login")
