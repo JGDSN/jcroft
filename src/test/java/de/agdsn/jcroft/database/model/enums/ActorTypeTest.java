76b279db0bf1bc972488a700ac25becbeddcd61d
JuKu <kuenzel.justin@t-online.de>
2018-07-26 00:09:39 +0200

    added some junit tests.

---
+++ src/test/java/de/agdsn/jcroft/database/model/enums/ActorTypeTest.java
@@ -0,0 +1,25 @@
+package de.agdsn.jcroft.database.model.enums;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ActorTypeTest {
+
+    @Test
+    public void testEnums () {
+        assertNotNull(ActorType.USER);
+        assertNotNull(ActorType.SERVICE);
+
+        assertEquals(2, ActorType.values().length);
+
+        assertEquals(ActorType.USER, ActorType.valueOf("USER"));
+        assertEquals(ActorType.SERVICE, ActorType.valueOf("SERVICE"));
+
+        //test for correct implementation of equals() and hashCode() method
+        assertEquals(ActorType.USER, ActorType.USER);
+        assertEquals(ActorType.SERVICE, ActorType.SERVICE);
+    }
+
+}
