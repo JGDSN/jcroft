10d2e6b3f32cf6b190dc5ca7df6844fafb0ed287
JuKu <kuenzel.justin@t-online.de>
2018-07-26 00:20:15 +0200

    added some junit tests for model User.

--- src/test/java/de/agdsn/jcroft/database/model/UserTest.java
+++ src/test/java/de/agdsn/jcroft/database/model/UserTest.java
@@ -2,6 +2,8 @@ package de.agdsn.jcroft.database.model;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -55,16 +57,21 @@ public class UserTest {
     @Test
     public void testGetterAndSetter () {
         User user = new User();
+        Date registered = new Date();
 
         user.setId(2);
         user.setFirstName("Max");
         user.setLastName("Mustermann");
         user.setUsername("max123");
+        user.setPasswordHash("my-password-hash");
+        user.setRegistered(registered);
 
         assertEquals(2, user.getId());
         assertEquals("Max", user.getFirstName());
         assertEquals("Mustermann", user.getLastName());
         assertEquals("max123", user.getUsername());
+        assertEquals("my-password-hash", user.getPasswordHash());
+        assertEquals(registered, user.getRegistered());
 
         assertNotNull(user.toString());
     }
