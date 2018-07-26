1046aa38572f6f732f796de41455ca4a81e41695
JuKu <kuenzel.justin@t-online.de>
2018-07-26 02:37:34 +0200

    added model Permission.

---
+++ src/main/java/de/agdsn/jcroft/database/model/Permission.java
@@ -0,0 +1,45 @@
+package de.agdsn.jcroft.database.model;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Entity
+@Table(name = "permissions")
+public class Permission implements Serializable {
+
+    @Size(max = 255)
+    @Id
+    @Column(name = "token", nullable = false, updatable = false, unique = true)
+    private String token;
+
+    @Column(name = "title", nullable = false, updatable = true)
+    private String title;
+
+    @Size(max = 45)
+    @Column(name = "description", nullable = false, updatable = true)
+    private String description;
+
+    public Permission (String token, String title, String description) {
+        this.token = token;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
