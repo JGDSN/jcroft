6472e7efe645e94881603146fe93788e0db3c947
JuKu <kuenzel.justin@t-online.de>
2018-07-31 18:20:21 +0200

    added some junit tests to test entity relationships of models User and Actor.

--- src/test/java/de/agdsn/jcroft/database/data/RepositoryTest.java
+++ src/test/java/de/agdsn/jcroft/database/data/RepositoryTest.java
@@ -46,6 +46,68 @@ public class RepositoryTest {
         assertNotNull(user1);
         assertEquals(user, user1);
         assertEquals(actor, user1.getActor());
+
+        //get actor
+        Optional<Actor> actorOptional = actorRepository.findById(user1.getActorId());
+        assertEquals(true, actorOptional.isPresent());
+        Actor actor1 = actorOptional.get();
+        assertEquals(user.getActor(), actor1);
+    }
+
+    @Test
+    public void testRemoveUser () {
+        //create new actor
+        Actor actor = new Actor(ActorType.USER);
+
+        //create new user
+        User user = new User("Max", "Mustermann", "test", "max@mustermann.de", actor);
+
+        actorRepository.save(actor);
+        userRepository.save(user);
+
+        //find user
+        Optional<User> userOptional = userRepository.findByUsername("test");
+        User user1 = userOptional.get();
+        assertEquals(true, userOptional.isPresent());
+
+        //remove user
+        userRepository.delete(user);
+
+        //find user again
+        Optional<User> userOptional1 = userRepository.findByUsername("test");
+        assertEquals(false, userOptional1.isPresent());
+
+        //check, if actor was also remove
+        Optional<Actor> actorOptional = actorRepository.findById(user1.getActorId());
+        assertEquals(false, actorOptional.isPresent());
+    }
+
+    @Test
+    public void testRemoveActor () {
+        //create new actor
+        Actor actor = new Actor(ActorType.USER);
+
+        //create new user
+        User user = new User("Max", "Mustermann", "test", "max@mustermann.de", actor);
+
+        actorRepository.save(actor);
+        userRepository.save(user);
+
+        //find user
+        Optional<User> userOptional = userRepository.findByUsername("test");
+        User user1 = userOptional.get();
+        assertEquals(true, userOptional.isPresent());
+
+        //remove actor
+        actorRepository.delete(actor);
+
+        //check, if actor was remove
+        Optional<Actor> actorOptional = actorRepository.findById(user1.getActorId());
+        assertEquals(false, actorOptional.isPresent());
+
+        //find user again
+        Optional<User> userOptional1 = userRepository.findByUsername("test");
+        assertEquals(true, userOptional1.isPresent());//TODO: set expected value to false
     }
 
 }
