8d47bfb371aa0893faac726322d197b55a6d810c
JuKu <kuenzel.justin@t-online.de>
2018-07-31 21:26:31 +0200

    fixed some problems in model MACAddress.

--- src/main/java/de/agdsn/jcroft/database/model/MACAddress.java
+++ src/main/java/de/agdsn/jcroft/database/model/MACAddress.java
@@ -1,12 +1,16 @@
 package de.agdsn.jcroft.database.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class MACAddress {
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Entity
+@Table(name = "mac_addresses")
+public class MACAddress implements Serializable {
 
     @Id
     @Column(name = "mac_address", nullable = false, updatable = false)
