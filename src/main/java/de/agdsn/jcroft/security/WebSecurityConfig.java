07c60965f2fa5bf987d3b4189abcd8673985e3af
FriwiDev <bblosaurus@web.de>
2018-08-08 16:55:12 +0200

    Update logout mechanism

--- src/main/java/de/agdsn/jcroft/security/WebSecurityConfig.java
+++ src/main/java/de/agdsn/jcroft/security/WebSecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.session.InvalidSessionStrategy;
-import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/**");
+        web.ignoring().antMatchers("/api/**", "/logoutSuccess", "/login", "/sessionExpired");
     }
 
     @Override
